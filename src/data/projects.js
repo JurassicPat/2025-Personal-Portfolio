e1cd565e14ef15c900ab8239bad8e33da608cb7c
Patrick Watertor <patrickwatertor.phone@gmail.com>
2025-07-17 00:11:22 -0500

    image issues

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,5 @@
 import project1Image from "../assets/frontline.webp";
-import project2Image from "../assets/vlct.webp";
+import project2Image from "../assets/VLCT.webp";
 import project3Image from "../assets/eden.webp";
 import project1HeroImage from "../assets/frontline-hero.webp";
 import project2HeroImage from "../assets/vlct-hero.webp";
