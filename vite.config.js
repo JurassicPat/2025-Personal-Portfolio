6462772e02e9737b5426615e52e600f45de29b2a
Patrick Watertor <patrickwatertor.phone@gmail.com>
2025-07-22 15:12:27 -0500

    compression

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import compression from 'vite-plugin-compression';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-   build: {
-    assetsInlineLimit: 0,},
-})
+  plugins: [
+    react(),
+
+    // Gzip compression (.gz)
+    compression({
+      algorithm: 'gzip',
+      ext: '.gz',
+      threshold: 10240, // Only compress files larger than 10kb
+    }),
+
+    // Brotli compression (.br)
+    compression({
+      algorithm: 'brotliCompress',
+      ext: '.br',
+      threshold: 10240,
+    }),
+  ],
+  build: {
+    assetsInlineLimit: 0, // Don't inline assets, useful for caching and compression
+  },
+});
